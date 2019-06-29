8a3a99c0dbac79fd675fdb0354b9ef995dcc42d8
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-29 00:16:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  document.querySelectorAll('.ranked-list li').forEach(function(li) {
+    li.innerHTML = parseInt(li.innerHTML) + n;
+  })
+}
+
+function deepestChild(){
+  let div = document.querySelector('div#grand-node')
+  while (div.querySelector('div')) {
+    div = div.querySelector('div');
+  }
+  return div;
+}
