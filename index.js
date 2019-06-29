30c7fc49c2a245a06a5bf3e6039d2ccefb7f6044
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-29 00:30:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  Array.prototype.forEach.call(document.querySelectorAll('.ranked-list li'), function(li) {
+  let lis = document.querySelectorAll('.ranked-list li');
+  Array.prototype.forEach.call(lis, function(li) {
     li.innerHTML = parseInt(li.innerHTML) + n;
   })
 }
